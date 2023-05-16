77a71bcbc9cd045a954dbc382bee46de65e0951a
vuongthai1 <kha.nguyen01.it@gmail.com>
2023-05-16 23:48:32 +0700

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,54 @@
-import { APP_CONSTANTS } from '../constants/commonConstants'
-import { CAREER_PAGE_PATH, EVENT_PAGE_PATH, HOME_PAGE_PATH, NEWS_PAGE_PATH, TALENT_PAGE_PATH } from '../constants/pathConstants'
-import Link from 'next/link'
+import { APP_CONSTANTS } from "@/constants/commonConstants";
+// import { mdiAccountOutline } from '@mdi/js';
 
-function Header() {
+import {
+  CAREER_PAGE_PATH,
+  EVENT_PAGE_PATH,
+  HOME_PAGE_PATH,
+  NEWS_PAGE_PATH,
+  TALENT_PAGE_PATH,
+} from "@/constants/pathConstants";
+import Link from "next/link";
+import "../styles/header.css";
+export default function Header() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <Link href="/">
-          <span className="navbar-brand">{APP_CONSTANTS.title}</span>
-        </Link>
+    <div className="container">
+      <header>
+        <div className="section-header">
+          <Link href="/">
+            <span className="navbar-brand">{APP_CONSTANTS.title}</span>
+          </Link>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link href={HOME_PAGE_PATH.path}>
-                <span className="nav-link">{HOME_PAGE_PATH.description}</span>
+          <ul className="header__list">
+            <li>
+              <Link className="header__item" href={HOME_PAGE_PATH.path}>
+                {HOME_PAGE_PATH.description}
               </Link>
             </li>
-            <li className="nav-item">
-            <Link href={NEWS_PAGE_PATH.path}>
+            <li>
+              <Link className="header__item" href={NEWS_PAGE_PATH.path}>
                 <span className="nav-link">{NEWS_PAGE_PATH.description}</span>
               </Link>
             </li>
-            <li className="nav-item">
-            <Link href={EVENT_PAGE_PATH.path}>
+            <li>
+              <Link className="header__item" href={EVENT_PAGE_PATH.path}>
                 <span className="nav-link">{EVENT_PAGE_PATH.description}</span>
               </Link>
             </li>
-            <li className="nav-item">
-            <Link href={CAREER_PAGE_PATH.path}>
+            <li>
+              <Link className="header__item" href={CAREER_PAGE_PATH.path}>
                 <span className="nav-link">{CAREER_PAGE_PATH.description}</span>
               </Link>
             </li>
-            <li className="nav-item">
-            <Link href={TALENT_PAGE_PATH.path}>
+            <li>
+              <Link className="header__item" href={TALENT_PAGE_PATH.path}>
                 <span className="nav-link">{TALENT_PAGE_PATH.description}</span>
               </Link>
             </li>
           </ul>
+          <div className="header__icon"></div>
         </div>
-      </div>
-    </nav>
-  )
+      </header>
+    </div>
+  );
 }
-
-export default Header
